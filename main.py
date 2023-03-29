ce3357bd078f7f103d65a1e8d414d6333e9dc256
hajimegen <tsuchihaji8701@yahoo.ne.jp>
2023-03-29 17:40:36 +0900

    11

---
+++ main.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+import json
+with open ('line.json') as f:
+    line_json = json.load(f)
+    
+import requests
+
+def notify_message(message):
+        LINE_NOTIFY_TOKEN = "v5umZRPB958apwnBiKy8w0dsykBrYVsALc5xDWMuWlV"
+        url = 'https://notify-api.line.me/api/notify'
+        #message = 'テスト'
+
+        headers = {
+            'Authorization': f'Bearer {LINE_NOTIFY_TOKEN}'
+        }
+
+        data = {
+            'message' : message
+        }
+
+        requests.post(
+            url,
+            headers = headers,
+            data = data
+        )
+
+def main():
+    message ='テストでっす'
+    notify_message(message)
+    
+if __name__ == "__main__":
+    main()
